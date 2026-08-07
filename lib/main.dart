bd07a7f58c407907f29fc0e22f99d2b6ce93b0ea
Shady Abdallah <shadyabdallah241@gmail.com>
2026-08-08 00:35:01 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/core/theme/light_mode.dart';
+import 'package:chat_app/features/login/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      theme: lightMode,
+      home: const LoginPage(),
+    );
+  }
+}
